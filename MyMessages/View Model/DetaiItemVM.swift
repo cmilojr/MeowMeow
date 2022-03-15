5f9120740d5e318fc6095055fe4f581c761c0a46
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2022-03-15 17:51:07 -0500

    prueba

---
+++ MyMessages/View Model/DetaiItemVM.swift
@@ -0,0 +1,46 @@
+//
+//  DetaiItemVM.swift
+//  MyMessages
+//
+//  Created by Camilo Jimenez on 13/09/21.
+//
+
+import Foundation
+
+
+struct DetailItemVM {
+    func getPokemonDetail(name: String, completion: @escaping (PokemonDetailModel?, Error?) -> Void) {
+        Networking.shared.get(URL(string: "")!) { (res: PokemonDetailModel?, error: Error?) in
+            if let err = error {
+                print(err.localizedDescription)
+                completion(nil, err)
+            } else if let pokemon = res {
+                completion(pokemon, nil)
+            }
+        }
+    }
+    
+    func savePokemonDetail(_ pokemonDetail: Description) throws {
+        do {
+            try Storage.shared.putPokemon(pokemonDescription: pokemonDetail)
+        } catch {
+            throw error
+        }
+    }
+        
+    func checkPokemon(_ info: Description) throws -> Bool {
+        do {
+            return try Storage.shared.checkPokemon(name: info.name)
+        } catch {
+            throw error
+        }
+    }
+    
+    func deletePokemon(_ info: Description) throws {
+        do {
+            try Storage.shared.deletePokemon(name: info.name)
+        } catch {
+            throw error
+        }
+    }
+}
